c3f1c7aa93adce12a2eb80220af63811fc0644b7
Shyam Mittal <30571441+nobi1007@users.noreply.github.com>
2019-03-17 03:30:36 +0530

    Update uploading.py

--- G-Drive/uploading.py
+++ G-Drive/uploading.py
@@ -2,17 +2,17 @@ import json
 import requests
 import cv2
 
-img = cv2.imread('clouds.jpg',1)
-cv2.imwrite('clouds1.jpg',img)
+img = cv2.imread('name of the downloaded image',0)
+cv2.imwrite('name of the image after editing',img)
 
 headers = {"Authorization": "Bearer (Your Auth Key without braces)"}
 para = {
-    "name": "clouds.jpg",
+    "name": "name to be displayed for the uploaded image",
     "parents": ["id of the g-drive folder from where the file has to be downloaded"]
 }
 files = {
     'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),
-    'file': ('image/jpg',open("./clouds1.jpg", "rb"))
+    'file': ('image/jpg',open("./name of the edited file", "rb"))
 }
 r = requests.post(
     "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
